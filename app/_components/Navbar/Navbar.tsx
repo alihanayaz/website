1f8fb2645cc9ccdd60b337984c4ef5df7f011fcf
alihanayaz <alihanayaz0@gmail.com>
2024-03-30 11:05:13 +0300

    feat: update styles and add menu button to navbar component

--- app/_components/Navbar/Navbar.tsx
+++ app/_components/Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 "use client";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import styles from "./Navbar.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import cn from "classnames";
 import { capitalizeFirstLetter } from "@/_lib/helpers";
-import { Profile } from "./Profile";
 
 export function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
 
   const routes = [
@@ -16,34 +17,52 @@ export function Navbar() {
     { label: "bookmarks", href: "/bookmarks" },
   ];
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    document.body.style.overflow = isMenuOpen ? "auto" : "hidden";
+  };
+
   return (
     <div className={styles.navbar}>
-      <Profile />
-      <div className={styles.list}>
-        {routes.map((link, i) => {
-          const isActive =
-            pathname === link.href || pathname.startsWith(`${link.href}/`);
-          return (
-            <Link
-              href={link.href}
-              className={cn(styles.listItem, {
-                [styles.active]: isActive,
-              })}
-              key={i}
-            >
-              <div className={styles.icon}>
-                <Image
-                  src={`/${link.label}.svg`}
-                  width={24}
-                  height={24}
-                  alt={link.label}
-                  priority={true}
-                ></Image>
-              </div>
-              <span>{capitalizeFirstLetter(link.label)}</span>
-            </Link>
-          );
-        })}
+      <div className={styles.menuButton} onClick={toggleMenu}>
+        <Image
+          src="/menu.svg"
+          width={32}
+          height={32}
+          alt="menu"
+          priority={true}
+        ></Image>
+      </div>
+      <div
+        className={cn(styles.list, { [styles.open]: isMenuOpen })}
+        onClick={toggleMenu}
+      >
+        <div className={styles.menuContainer}>
+          {routes.map((link, i) => {
+            const isActive =
+              pathname === link.href || pathname.startsWith(`${link.href}/`);
+            return (
+              <Link
+                href={link.href}
+                className={cn(styles.listItem, {
+                  [styles.active]: isActive,
+                })}
+                key={i}
+              >
+                <div className={styles.icon}>
+                  <Image
+                    src={`/${link.label}.svg`}
+                    width={24}
+                    height={24}
+                    alt={link.label}
+                    priority={true}
+                  ></Image>
+                </div>
+                <span>{capitalizeFirstLetter(link.label)}</span>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
