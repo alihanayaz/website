63f5ea00f83c0ff801c0040e845438393b2bee43
alihanayaz <alihanayaz0@gmail.com>
2024-04-08 21:57:33 +0300

    refactor: optimize grid and card styles

--- app/(routes)/journey/page.tsx
+++ app/(routes)/journey/page.tsx
@@ -4,6 +4,7 @@ import Timeline from "@/_components/Timeline";
 export default function Page() {
   return (
     <Section>
+      <h3>My Journey So Far</h3>
       <Timeline />
     </Section>
   );
--- app/_components/Timeline/Timeline.tsx
+++ app/_components/Timeline/Timeline.tsx
@@ -1,11 +1,9 @@
-import styles from "./Timeline.module.scss";
 import TimelineItem from "./TimelineItem";
 import { TIMELINE_CONTENT } from "@/_lib/constants";
 
 export function Timeline() {
   return (
-    <div className={styles.timelineContainer}>
-      <h3>My Journey So Far</h3>
+    <div>
       {TIMELINE_CONTENT.map((item, i) => (
         <TimelineItem key={i} item={item} />
       ))}
