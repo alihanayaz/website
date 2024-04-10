5a557eec538624ed82f28a46239f08e186d02be3
alihanayaz <alihanayaz0@gmail.com>
2024-04-11 01:42:40 +0300

    feat: add image support to content and update styles

--- app/_components/PortableContent/PortableContent.tsx
+++ app/_components/PortableContent/PortableContent.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {
   PortableText,
-  PortableTextComponents,
-  PortableTextBlock,
-  PortableTextComponentProps,
+  type PortableTextComponents,
+  type PortableTextBlock,
+  type PortableTextComponentProps,
 } from "@portabletext/react";
 import { slugify } from "@/_lib/helpers";
+import { urlFor } from "@/_lib/sanity/query";
+import styles from "./PortableContent.module.scss";
 
 export const PortableContent = ({
   content,
@@ -20,15 +22,43 @@ export const PortableContent = ({
     const slug = slugify(joinedChildren);
     const href = `#${slug}`;
     return (
-      <h3 id={slug}>
+      <h2 id={slug} className={styles.title}>
         <a href={href}>{children}</a>
-      </h3>
+      </h2>
     );
   };
 
   const components: PortableTextComponents = {
     block: {
-      h3: LinkableHeader,
+      h2: LinkableHeader,
+    },
+    types: {
+      image: ({ value }) => {
+        return (
+          <img
+            src={urlFor(value.asset).url()}
+            alt={value.alt}
+            className={styles.image}
+          />
+        );
+      },
+    },
+    marks: {
+      link: ({ children, value }) => {
+        const rel = !value.href.startsWith("/")
+          ? "noreferrer noopener"
+          : undefined;
+        return (
+          <a
+            href={value.href}
+            rel={rel}
+            target="_blank"
+            className={styles.link}
+          >
+            {children}
+          </a>
+        );
+      },
     },
   };
 
--- app/_components/Timeline/TimelineItem.tsx
+++ app/_components/Timeline/TimelineItem.tsx
@@ -1,17 +1,23 @@
 import styles from "./TimelineItem.module.scss";
 import { TimelineProps } from "@/_lib/sanity/types";
+import { urlFor } from "@/_lib/sanity/query";
 
 export default function TimelineItem({ item }: { item: TimelineProps }) {
-  const { title, duration, detail } = item;
+  const { title, duration, detail, image } = item;
   return (
     <ol className={styles.wrapper}>
       <li className={styles.content}>
         <div className={styles.bulletPoint}></div>
         <div className={styles.heading}>
           <span className={styles.title}>{title}</span>
-          <span className={styles.duration}>{duration}</span>
+          {duration && <span className={styles.duration}>{duration}</span>}
         </div>
         <p className={styles.detail}>{detail}</p>
+        {image && (
+          <div className={styles.imageContainer}>
+            <img src={urlFor(image).url()} alt={title} />
+          </div>
+        )}
       </li>
     </ol>
   );
